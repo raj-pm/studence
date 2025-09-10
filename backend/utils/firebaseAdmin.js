8a641ae143f74863e192854733f6d09379b5b960
raj-pm <poralrajeshwari@example.com>
2025-09-10 05:49:49 +0530

    env change

--- backend/utils/firebaseAdmin.js
+++ backend/utils/firebaseAdmin.js
@@ -2,7 +2,7 @@ import admin from 'firebase-admin';
 import { readFileSync } from 'fs';
 import path from 'path';
 
-const serviceAccountPath = path.resolve('./firebase-service-account.json');
+const serviceAccountPath = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 const serviceAccount = JSON.parse(readFileSync(serviceAccountPath, 'utf8'));
 
 if (!admin.apps.length) {
